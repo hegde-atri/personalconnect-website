c3a38fa1efbee2ec61297b7805d45eda37e90aa9
Atri Hegde <iamatrihegde@outlook.com>
2023-09-14 16:29:18 +0100

    fixed merge conflict

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import { useRouter } from "next/navigation";
 import type { Database } from "@/lib/supabase";
 import { useState } from "react";
 export default function Home() {
-<<<<<<< Updated upstream
   const [name, setName] = useState<string>("");
   const router = useRouter();
   const supabase = createClientComponentClient<Database>();
@@ -16,24 +15,12 @@ export default function Home() {
     });
   };
   getName();
-=======
-    const [,] = useState(null);
-    const router = useRouter();
-    const supabase = createClientComponentClient<Database>();
-    const getUser = async () => {
-        await supabase.auth.getSession().then((res) => {
-            if (res.data.session != undefined) {
-                setUser(res.data.session?.user);
-            });
-    };
->>>>>>> Stashed changes
 
-    const handleSignIn = async () => {
-        await supabase.auth.signInWithOAuth({ provider: "google" });
-        router.refresh();
-    };
+  const handleSignIn = async () => {
+    await supabase.auth.signInWithOAuth({ provider: "google" });
+    router.refresh();
+  };
 
-<<<<<<< Updated upstream
   const handleSignOut = async () => {
     await supabase.auth.signOut();
     router.refresh();
@@ -65,37 +52,4 @@ export default function Home() {
       </div>
     </main>
   );
-=======
-    const handleSignOut = async () => {
-        await supabase.auth.signOut();
-        router.refresh();
-    };
-    return (
-        <main className="flex flex-col items-center">
-            <div className="text-5xl mx-auto text-center font-bold mt-28 mb-5">
-                <h1>PersonalConnect</h1>
-            </div>
-            <div className="mt-5 flex flex-col">
-                {name ? (
-                    <div className="flex flex-col mx-auto items-center">
-                        <p className="text-2xl items-center">Hey {name}!</p>
-                        <button
-                            className="rounded-md mt-5 py-2 px-3 bg-slate-900 hover:bg-slate-800 transition duration-300"
-                            onClick={handleSignOut}
-                        >
-                            Sign out
-                        </button>
-                    </div>
-                ) : (
-                    <button
-                        className="rounded-md mt-5 py-2 px-3 bg-slate-900 hover:bg-slate-800 transition duration-300"
-                        onClick={handleSignIn}
-                    >
-                        Sign in
-                    </button>
-                )}
-            </div>
-        </main>
-    );
->>>>>>> Stashed changes
 }
